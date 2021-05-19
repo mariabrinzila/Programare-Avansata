65d522adad2a499cb1e4d03f986159c56e7b2c8e
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2021-05-19 23:32:33 +0300

    Update ExceptionHandler.java

--- Laboratorul 11/optional/ExceptionHandler.java
+++ Laboratorul 11/optional/ExceptionHandler.java
@@ -1,9 +1,7 @@
 package programareavansata.laboratorul11.utils;
 
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
 
